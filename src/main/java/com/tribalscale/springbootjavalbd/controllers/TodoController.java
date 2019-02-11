60bae62ab95dce32c308606c1918bec4688e5101
Jie <j42lin@users.noreply.github.com>
2019-02-10 21:32:53 -0500

    Refactor controller

--- src/main/java/com/tribalscale/springbootjavalbd/controllers/TodoController.java
+++ src/main/java/com/tribalscale/springbootjavalbd/controllers/TodoController.java
@@ -1,34 +1,51 @@
 package com.tribalscale.springbootjavalbd.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Arrays;
-import com.tribalscale.springbootjavalbd.models.Todo;
-import com.tribalscale.springbootjavalbd.services.TodoService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+import com.tribalscale.springbootjavalbd.models.Todo;
+import com.tribalscale.springbootjavalbd.services.TodoService;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
 
 @RestController
+@RequestMapping(path = "/todo")
 public class TodoController {
   private TodoService todoService;
   @Autowired
   public TodoController(TodoService todoService) {
     this.todoService = todoService;
   }
-  @GetMapping(value="/")
-  public String get() {
-    return "helloworld";
-  }
 
-  @GetMapping(value="/todo")
+  @GetMapping
   public List<Todo> getTodos() {
     return todoService.getTodoList();
   }
   
-  @PostMapping(value="/todo")
-  public List<Todo> postMethodName(@RequestBody String text, int priority) {
+  @PostMapping
+  public List<Todo> postMethodName(@Valid @RequestBody TodoBody todoBody) {
       //TODO: process POST request
-      return todoService.createTodo(text, priority);
+      return todoService.createTodo(todoBody.getText(), todoBody.getPriority());
   } 
+}
+
+@JsonRootName("todo")
+@Getter
+@NoArgsConstructor
+class TodoBody {
+  @NotBlank (message = "can't be blank")
+  private String text;
+  @NotBlank (message = "can't be blank")
+  private int priority;
 }
